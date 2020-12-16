61d0c147646ce2b0a96db8760a2f6d38d74634d9
Schuyler Lawrence <schuyler.lawrence@gmail.com>
2020-12-15 16:12:37 -0800

    scrolling story

--- src/Detail.js
+++ src/Detail.js
@@ -12,6 +12,7 @@ async function callApi(film) {
         y: res.data.release_date.substring(0, 4),
       },
     });
+    console.log(res.data);
     return omdb.data.Poster;
   } catch (err) {
     console.error("There was a problem fetching:", err);
@@ -66,6 +67,18 @@ export default function Detail({ person }) {
             );
           })}
         </ul>
+        <div className="marquee">
+          <div className="text">
+            "It is a period of civil war. Rebel spaceships, striking from a
+            hidden base, have won their first victory against the evil Galactic
+            Empire. During the battle, Rebel spies managed to steal secret plans
+            to the Empire's ultimate weapon, the DEATH STAR, an armored space
+            station with enough power to destroy an entire planet. Pursued by
+            the Empire's sinister agents, Princess Leia races home aboard her
+            starship, custodian of the stolen plans that can save her people and
+            restore freedom to the galaxy...."
+          </div>
+        </div>
       </div>
     </div>
   );
